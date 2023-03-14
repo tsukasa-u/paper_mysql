cd952714ba409ef7597eec3189f4671d5de29656
tsukasa-u <68815462+tsukasa-u@users.noreply.github.com>
2023-03-15 00:24:04 +0900

    Create CommandClass.java

---
+++ src/main/java/io/tsukasau/paper_mysql/CommandClass.java
@@ -0,0 +1,27 @@
+package io.tsukasau.paper_mysql;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.SQLException;
+
+public class CommandClass implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, Command command, @NotNull String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("updateInventory")) {
+            if (args.length == 0) {
+
+                Player player = (Player) sender;
+
+                inventory_sync.statusRecord.loadPlayer(player, "ENFORCE");
+                sender.sendMessage("enforced to update inventory!, " + player.getName() + "!");
+
+                return true;
+            }
+        }
+        return false;
+    }
+}
